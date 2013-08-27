05e25390c345d4816473fcfd67a50cd21c88b8be
abhinav narain <nabhinav3@gatech.edu>
2013-08-27 15:12:00 +0000

    making calc specific to data frames

--- contention-calc.py
+++ contention-calc.py
@@ -213,144 +213,20 @@ for data_f_name_list in filename_list : #data_fs :
         del frame_elem
         del monitor_elem
 
-    #The following code block parses the mgmt files     
-    beacon_mgmt_frames=header_and_beacon_mgmt_frames[mgmt_file_header_byte_count+1:]
-    mgmt_index=0
-    for idx in xrange(0,len(beacon_mgmt_frames)-MGMT_BEACON_STRUCT_SIZE ,MGMT_BEACON_STRUCT_SIZE ):
-        #global file_timestamp
-        frame=beacon_mgmt_frames[mgmt_index:mgmt_index+MGMT_BEACON_STRUCT_SIZE]
-        offset,success,tsf= 8,-1,0
-        header = frame[:offset]
-        frame_elem,monitor_elem=defaultdict(list),defaultdict(list)
-        (version,pad,radiotap_len,present_flag)=struct.unpack('<BBHI',header)
-        if not( radiotap_len ==RADIOTAP_RX_LEN or  radiotap_len == RADIOTAP_TX_LEN) :
-            print "the radiotap header is not correct "
-            sys.exit(1)
-        (success,frame_elem,monitor_elem)=parse_radiotap(frame,radiotap_len,present_flag,offset,monitor_elem,frame_elem)
-        if success :
-            for key in frame_elem.keys():
-                tsf=key
-            temp=frame_elem[tsf]
-            temp.insert(0,tsf)
-            parse_mgmt_beacon_frame(frame,radiotap_len,frame_elem)
-            if radiotap_len== RADIOTAP_TX_LEN :
-                tx_time_mgmt_series.append(temp)
-                print temp                
-        else :
-            print "beacon success denied"
-
-        mgmt_index=mgmt_index+MGMT_BEACON_STRUCT_SIZE
-        del frame_elem
-        del monitor_elem
-
-    mgmt_index=0
-    for idx in xrange(0,len(common_mgmt_frames)-MGMT_COMMON_STRUCT_SIZE,MGMT_COMMON_STRUCT_SIZE ):
-        #global file_timestamp
-        frame=common_mgmt_frames[mgmt_index:mgmt_index+MGMT_COMMON_STRUCT_SIZE]
-        offset,success,tsf= 8,-1,0
-        header = frame[:offset]
-        frame_elem,monitor_elem=defaultdict(list),defaultdict(list)
-        (version,pad,radiotap_len,present_flag)=struct.unpack('<BBHI',header)
-        if not( radiotap_len ==RADIOTAP_RX_LEN or  radiotap_len == RADIOTAP_TX_LEN) :
-            print "the radiotap header is not correct "
-            sys.exit(1)
-        (success,frame_elem,monitor_elem)=parse_radiotap(frame,radiotap_len,present_flag,offset,monitor_elem,frame_elem)
-        if success==1 :
-            for key in frame_elem.keys():
-                tsf=key
-            temp=frame_elem[tsf]
-            temp.insert(0,tsf)
-            parse_mgmt_beacon_frame(frame,radiotap_len,frame_elem)
-            if radiotap_len== RADIOTAP_TX_LEN :
-                tx_time_mgmt_series.append(temp)
-        else :
-            print "common mgmt success denied"
-
-        mgmt_index= mgmt_index+MGMT_COMMON_STRUCT_SIZE
-        del frame_elem
-        del monitor_elem
-    #print "----------done with missed .. now with actual ctrl data "        
-    correct_ctrl_frames=header_and_correct_ctrl_frames[ctrl_file_header_byte_count+1:]
-    ctrl_index=0
-    for idx in xrange(0,len(correct_ctrl_frames)-CTRL_STRUCT_SIZE ,CTRL_STRUCT_SIZE ):			
-        #global file_timestamp
-        frame=correct_ctrl_frames[ctrl_index:ctrl_index+CTRL_STRUCT_SIZE]
-        offset,success,tsf= 8,-1,0
-        header = frame[:offset]
-        frame_elem, monitor_elem=defaultdict(list),defaultdict(list)
-        (version,pad,radiotap_len,present_flag)=struct.unpack('<BBHI',header)
-        if not( radiotap_len ==RADIOTAP_RX_LEN or radiotap_len == RADIOTAP_TX_LEN) :
-            print "the radiotap header is not correct "		
-            sys.exit(1)
-        (success,frame_elem,monitor_elem)=parse_radiotap(frame,radiotap_len,present_flag,offset,monitor_elem,frame_elem)
-        if success :
-            for key in frame_elem.keys():
-                tsf=key
-            parse_ctrl_frame(frame,radiotap_len,frame_elem)
-            temp=frame_elem[tsf]
-            temp.insert(0,tsf)
-            if radiotap_len ==RADIOTAP_TX_LEN :
-                tx_time_ctrl_series.append(temp)
-            elif radiotap_len ==RADIOTAP_RX_LEN :
-                rx_time_ctrl_series.append(temp)
-        else :
-            print "ctrl success denied"
-        
-        ctrl_index=ctrl_index+CTRL_STRUCT_SIZE
-        
-        del frame_elem
-        del monitor_elem
-
     file_counter +=1
     if file_counter %10 == 0:
         print file_counter
 
-
 print "now processing the files to calculate time "
-
 tx_time_data_series.sort(key=lambda x:x[0])
-tx_time_ctrl_series.sort(key=lambda x:x[0])
-tx_time_mgmt_series.sort(key=lambda x:x[0])
 
 dic= defaultdict(list) 
-mgmt_zero_tx=[]
-mgmt_retx_count=[]
-
-ctrl_zero_tx=[]
-ctrl_retx_count=[]
-
 data_zero_tx=[]
 data_retx_count=[]
 
 Station_list=list(Station)
 Station_tx_retx_count = defaultdict(list)
-
-for i in range(0,len(tx_time_ctrl_series)):
-    frame=tx_time_ctrl_series[i]
-    c_frame_tx_flags_radiotap=frame[1]
-    c_frame_retx=frame[2]
-    c_frame_mpdu_queue_size=frame[5]
-    c_frame_retx=frame[2]
-    c_frame_total_time=frame[4]
-    if c_frame_tx_flags_radiotap[0]==0 and not(c_frame_retx ==0):
-        dic['ctrl_retx_count'].append(c_frame_retx)
-
-    if c_frame_mpdu_queue_size ==0 and c_frame_retx==0 :
-        dic['ctrl_zero_tx'].append(c_frame_total_time)
         
-        
-for i in range(0,len(tx_time_mgmt_series)):
-    frame=tx_time_mgmt_series[i]
-    c_frame_tx_flags_radiotap=frame[1]
-    c_frame_retx=frame[2]
-    c_frame_mpdu_queue_size=frame[5]
-    c_frame_retx=frame[2]
-    c_frame_total_time=frame[4]
-    if c_frame_tx_flags_radiotap[0]==0 and not(c_frame_retx ==0):
-        dic['mgmt_retx_count'].append(c_frame_retx)
-    if c_frame_mpdu_queue_size ==0 and c_frame_retx==0 :
-        dic['mgmt_zero_tx'].append(c_frame_total_time)
-
 for i in range(0,len(tx_time_data_series)):
     frame = tx_time_data_series[i]
     c_frame_tx_flags_radiotap=frame[1]
