e12b65f83574dc8831b4256facbfd34be2fd1b5c
abhinav narain <abhinavnarain10@gmail.com>
2013-09-22 22:21:10 +0000

    cleaning previous code ; adding initial code that reads the ap counts and contention periods

--- scatterplot.py
+++ scatterplot.py
@@ -1,45 +1,54 @@
 #Author : Abhinav Narain
 #Date : 5-sept-2013
-#Purpose : To plot the devices inside homes 
+#Purpose : To plot the scatter plot of bitrates observed in a home
+# vs the wireless contention inside homes 
 
 
 from magicplott import *
 
-'''
-testing dataset :
-retx_rate_table = {
+def pickle_reader(input_folder,threshold):
+
+    '''
+    testing dataset format :
+    retx_rate_table = {
     'OWC43DC7B0AE78' : [[54.0,1],[36.0,2],[5.5,3],],
     'OWC43DC7A3EDEC' : [[54.0,1],[36.0,2],[5.5,3],],
     'OWC43DC7A3EE22' :[[54.0,3],[48.0,1],[5.5,4],],        
     }
-
-contention_table = {
+    
+    contention_table = {
     'OWC43DC7B0AE78' : [157,13.3,312,523,123,5235,55111,2424,54],
     'OWC43DC7A3EDEC' : [155,123.3,312,23,121,3523,14235,2424,554],
     'OWC43DC7A3EE22' : [173,123.3,312,523,123,5235,12455,2424,5254],
     }
-'''
-def retx_map(_tx,_retx,threshold,router_id):
-    retx_list_list=[]
-    for k,v in _tx.iteritems():
-        if k in _retx :
-            if v> threshold:
-                ratio=_retx[k]*1.0/v
-                retx_list_list.append([k,ratio])
+    '''
+
+    '''
+    reads the pickle file by contention-data-frame-calc to fetch required data
+    '''    
+    def retx_map(_tx,_retx,threshold):
+        '''
+        transmforms the counts to a fraction if the frame
+        count is above a threshold
+        '''
+        retx_list_list=[]
+        for k,v in _tx.iteritems():
+            if k in _retx :
+                if v> threshold:
+                    ratio=_retx[k]*1.0/v
+                    retx_list_list.append([k,ratio])
+                else :
+                    pass
             else :
-                pass
-        else :
-            if v> threshold:
-                retx_list_list.append([k,0.0])
-    for k,v in _retx.iteritems():
-        if not (k in _tx):
-            if v> threshold:
-                retx_list_list.append([k,v*1.0])
-    return retx_list_list
+                if v> threshold:
+                    retx_list_list.append([k,0.0])
+        for k,v in _retx.iteritems():
+            if not (k in _tx):
+                if v> threshold:
+                    retx_list_list.append([k,v*1.0])
 
+        return retx_list_list
 
-def pickle_reader(input_folder,threshold):
-    print "the pickle reader called " 
     data_fs=os.listdir(input_folder)
     c_table={}
     undersampled_rate_list=[]
@@ -51,7 +60,7 @@ def pickle_reader(input_folder,threshold):
 	transmission_count_table=_f_content[2]
 	contention_time=_f_content[3]
 	c_table[router_id]=contention_time
-        l=retx_map(transmission_count_table,retransmission_count_table,threshold,router_id)
+        l=retx_map(transmission_count_table,retransmission_count_table,threshold)
         if len(l)==0:
             print router_id
             print len(l)
@@ -61,7 +70,44 @@ def pickle_reader(input_folder,threshold):
 
     return [c_table,retx_norm_table,undersampled_rate_list]	
 
-if __name__=='__main__':    
+
+def contention_data_pickle_reader(contention_data_input_folder):
+    '''
+    reads the pickle file by contention-data-frame-calc to fetch
+    only the contention period of devices
+    '''
+    data_fs=os.listdir(contention_data_input_folder)
+    home_contention_table=defaultdict(list)
+    for f_name in data_fs :
+        #router_id,ap_macs,device_macs,ap_map,device_map,rate_map ; maps are of times 
+	_f_content= pickle.load(open(contention_data_input_folder+f_name,'rb'))
+	router_id= _f_content[0]
+        retransmission_count_table=_f_content[1]
+        frame_count_table=_f_content[2]
+        contention_time=_f_content[3]
+        home_contention_table[router_id]=contention_time
+    return home_contention_table
+
+def device_count_pickle_reader(input_folder):
+    '''
+    fetches the data from pickle file dumped by device-count-process.py
+    '''
+    data_fs=os.listdir(input_folder)
+    home_device_table=defaultdict(list)
+    home_ap_table=defaultdict(list)
+    for f_name in data_fs :
+        #router_id,ap_macs,device_macs,ap_map,device_map,rate_map ; maps are of times 
+	_f_content= pickle.load(open(input_folder+f_name,'rb'))
+	router_id= _f_content[0]
+        ap_mac=_f_content[1]
+        device_mac=_f_content[2]
+        home_device_table[router_id]=device_mac
+        home_ap_table[router_id]=ap_mac
+        
+    return [home_ap_table,home_device_table]
+
+
+if 0: #__name__=='__main__':    
     if len(sys.argv) !=3:
         print "usage : python unpickeler.py data_folder filename.png  "
         sys.exit(0)
@@ -88,10 +134,30 @@ if __name__=='__main__':
     print retx_rate_table
     
     plotter_scatter(retx_rate_table,
-        contention_table,
-        'retransmits(no. of frames retransmitted /no. of successful transmissions)',
-        'Contention Delay(microsecond) 90th percentile',
+                    contention_table,
+                    'retransmits(no. of frames retransmitted /no. of successful transmissions)',
+                    'Contention Delay(microsecond) 90th percentile',
                     0,
                     0,
                     'Scatterplot for retransmission vs Contention Delay in homes for 5GHz band',
                     outfile_name)
+
+
+if __name__ == '__main__':                                                                                             
+    '''
+    Plot the Contention time delay vs the Number of Access Points
+    '''
+    if len(sys.argv) !=4:
+        print "usage : python unpickeler.py <contention_data_folder_2GHz>  <ap_count_data_folder> <filename.png>  "
+        sys.exit(0)
+    contention_data_input_folder = sys.argv[1]
+    access_point_data_input_folder = sys.argv[2]
+    outfile_name = sys.argv[3]
+    home_ap_2_table=defaultdict(list)
+    home_device_2_table=defaultdict(list)
+    home_contention_table=defaultdict(list)
+    [home_ap_2_table,home_device_2_table]=device_count_pickle_reader(access_point_data_input_folder)
+    home_contention_table=contention_data_pickle_reader(contention_data_input_folder)
+    
+#    simply_scatter(         )
+    #print home_ap_2_table
