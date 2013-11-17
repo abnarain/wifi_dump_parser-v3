8b32b918528e67e5e9d140b1e2dc5873dcaf2efe
abhinav narain <abhinavnarain10@gmail.com>
2013-11-17 00:36:09 +0000

    code for processing rates separated to be from multicast, unicast and of badfcs packet

--- rate_distribution.py
+++ rate_distribution.py
@@ -31,14 +31,12 @@ def all_devices_rates_file_reader(t1,t2,data_fs):
     file_count=0
     for data_f_n in data_fs :
         filename_list.append(data_f_n.split('-'))
-        unix_time.add(int(data_f_n.split('-')[1]))
         if not (data_f_n.split('-')[2]=='d'):
             print "its not a data file ; skip "
             continue 
 
     filename_list.sort(key=lambda x : int(x[3]))
     filename_list.sort(key=lambda x : int(x[1]))
-    tt=0
     for data_f_name_list in filename_list : #data_fs :    
         file_count=file_count+1
         data_f_name="-".join(data_f_name_list)
@@ -63,8 +61,6 @@ def all_devices_rates_file_reader(t1,t2,data_fs):
         data_contents=data_file_content.split('\n----\n')
         header_and_correct_data_frames = data_contents[0]
         err_data_frames = data_contents[1]
-        correct_data_frames_missed=data_contents[2]
-        err_data_frames_missed=data_contents[3]
 
     #done with reading the binary blobs from file ; now check for timestamps are correct
         '''
@@ -86,7 +82,6 @@ def all_devices_rates_file_reader(t1,t2,data_fs):
             if success==1:
                 for key in frame_elem.keys():
                     tsf=key                                    
-                    parse_data_frame(frame,radiotap_len,frame_elem)
 #TODO : fix the multicast and broadcast bitrates to be excluded
                     if radiotap_len ==RADIOTAP_RX_LEN :
                         rate_distribution[frame_elem[tsf][7]] +=1
@@ -113,7 +108,6 @@ def all_devices_rates_file_reader(t1,t2,data_fs):
             if success==1:
                 for key in frame_elem.keys():
                     tsf=key
-                parse_err_data_frame(frame,radiotap_len,frame_elem)
                 if radiotap_len == RADIOTAP_RX_LEN:                                    
                     rate_distribution[frame_elem[tsf][7]] +=1
                 elif radiotap_len ==RADIOTAP_TX_LEN :
@@ -136,19 +130,18 @@ tx_timeseries,rx_timeseries=[],[]
 def connected_devices_rates_file_reader(t1,t2,data_fs):
     '''
     Fetches the bitrate of each device from a home (uplink/downlink)
+    Used for plotting the bar graphs for uplink and downlink rates  
     '''
     global damaged_frames
     file_count=0
     for data_f_n in data_fs :
         filename_list.append(data_f_n.split('-'))
-        unix_time.add(int(data_f_n.split('-')[1]))
         if not (data_f_n.split('-')[2]=='d'):
             print "its not a data file ; skip "
             continue 
 
     filename_list.sort(key=lambda x : int(x[3]))
     filename_list.sort(key=lambda x : int(x[1]))
-    tt=0
     for data_f_name_list in filename_list : #data_fs :    
         file_count=file_count+1
         data_f_name="-".join(data_f_name_list)
@@ -257,14 +250,12 @@ def connected_devices_updown_rates_file_reader(t1,t2,data_fs):
     file_count=0
     for data_f_n in data_fs :
         filename_list.append(data_f_n.split('-'))
-        unix_time.add(int(data_f_n.split('-')[1]))
         if not (data_f_n.split('-')[2]=='d'):
             print "its not a data file ; skip "
             continue 
 
     filename_list.sort(key=lambda x : int(x[3]))
     filename_list.sort(key=lambda x : int(x[1]))
-    tt=0
     for data_f_name_list in filename_list : #data_fs :    
         file_count=file_count+1
         data_f_name="-".join(data_f_name_list)
@@ -514,6 +505,138 @@ def per_station_packet_dumper(t1,t2,data_fs,outfolder_name,router_id):
     pickle.dump(pickle_object,output_device)
     output_device.close()
 
+rate_without_multicast=defaultdict(int)
+rate_with_multicast=defaultdict(int)
+rate_distribution_err=defaultdict(int)
+def all_devices_rates_with_multicast_file_reader(t1,t2,data_fs):
+    '''
+    Fetches all the birates in a home in a data structure from file
+    '''
+    global damaged_frames
+    file_count=0
+    for data_f_n in data_fs :
+        filename_list.append(data_f_n.split('-'))
+        if not (data_f_n.split('-')[2]=='d'):
+            print "its not a data file ; skip "
+            continue 
+
+    filename_list.sort(key=lambda x : int(x[3]))
+    filename_list.sort(key=lambda x : int(x[1]))
+    for data_f_name_list in filename_list : #data_fs :    
+        file_count=file_count+1
+        data_f_name="-".join(data_f_name_list)
+        data_f= gzip.open(data_f_dir+data_f_name,'rb')
+        data_file_content=data_f.read()
+        data_f.close()
+        data_file_current_timestamp=0
+        data_file_seq_n=0
+        bismark_id_data_file=0
+        start_64_timestamp_data_file=0
+        for i in xrange(len(data_file_content )):
+            if data_file_content[i]=='\n':
+                bismark_data_file_header = str(data_file_content[0:i])
+                ents= bismark_data_file_header.split(' ')
+                bismark_id_data_file=ents[0]
+                start_64_timestamp_data_file= int(ents[1])
+                data_file_seq_no= int(ents[2])
+                data_file_current_timestamp=int(ents[3])
+                data_file_header_byte_count =i
+                break
+
+        data_contents=data_file_content.split('\n----\n')
+        header_and_correct_data_frames = data_contents[0]
+        err_data_frames = data_contents[1]
+
+    #done with reading the binary blobs from file ; now check for timestamps are correct
+        '''
+        if  (data_file_current_timestamp < t1-1):
+            continue
+        if (data_file_current_timestamp >t2+1):
+            break
+        '''
+        correct_data_frames=header_and_correct_data_frames[data_file_header_byte_count+1:]
+        data_index=0
+        for idx in xrange(0,len(correct_data_frames)-DATA_STRUCT_SIZE ,DATA_STRUCT_SIZE ):
+            frame=correct_data_frames[data_index:data_index+DATA_STRUCT_SIZE]
+            offset,success,tsf= 8,-1,0
+            header = frame[:offset]
+            frame_elem=defaultdict(list)
+            monitor_elem=defaultdict(list)
+            (version,pad,radiotap_len,present_flag)=struct.unpack('<BBHI',header)
+            (success,frame_elem,monitor_elem)=parse_radiotap(frame,radiotap_len,present_flag,offset,monitor_elem,frame_elem)  
+            tsf=0
+            if success==1:
+                for key in frame_elem.keys():
+                    tsf=key
+                parse_data_frame(frame,radiotap_len,frame_elem)
+                if radiotap_len ==RADIOTAP_RX_LEN :
+                    a= frame_elem[tsf][12].split(':')
+                    if not( int(a[0],16)& 0x1):
+                        rate_without_multicast[frame_elem[tsf][7]] +=1
+                    else :
+                        rate_with_multicast[frame_elem[tsf][7]] +=1
+                elif radiotap_len==RADIOTAP_TX_LEN :
+                    a= frame_elem[tsf][11].split(':')
+                    if not( int(a[0],16)& 0x1):
+                        rate_without_multicast[frame_elem[tsf][2]] +=1
+                    else:
+                        rate_with_multicast[frame_elem[tsf][2]] +=1 
+                else :
+                     print "data frame: impossible radiotap len"
+            else:
+                print "success denied; incorrect data frame"
+                damaged_frames +=1
+            data_index=data_index+DATA_STRUCT_SIZE
+            del frame_elem
+            del monitor_elem
+
+        data_index=0
+        for idx in xrange(0,len(err_data_frames)-DATA_ERR_STRUCT_SIZE,DATA_ERR_STRUCT_SIZE ):
+            frame=err_data_frames[data_index:data_index+DATA_ERR_STRUCT_SIZE]
+            offset,success,tsf= 8,-1,0
+            header = frame[:offset]
+            frame_elem=defaultdict(list)
+            monitor_elem=defaultdict(list)
+            (version,pad,radiotap_len,present_flag)=struct.unpack('<BBHI',header)
+            (success,frame_elem,monitor_elem)=parse_radiotap(frame,radiotap_len,present_flag,offset,monitor_elem,frame_elem)
+            if success==1:
+                for key in frame_elem.keys():
+                    tsf=key
+                if radiotap_len == RADIOTAP_RX_LEN:
+                    rate_distribution_err[frame_elem[tsf][7]] +=1
+                elif radiotap_len ==RADIOTAP_TX_LEN :
+                    print "err tx", frame_elem
+                    sys.exit(1)
+                else :
+                    print "impossible radiotap len detected ; Report CERN" 
+            else :
+                print "success denied; incorrect data frame"        
+            data_index= data_index+DATA_ERR_STRUCT_SIZE
+            del frame_elem
+            del monitor_elem
+
+        if file_count %10 == 0:
+            print file_count
+
+
+def all_devices_rates_with_multicast_dumper(t1,t2,outfolder_name,data_fs):
+    '''
+    Dumps the rate histograms for the number home
+    '''
+    all_devices_rates_with_multicast_file_reader(t1,t2,data_fs)
+    pickle_object= []
+    pickle_object.append(router_id)
+
+    pickle_object.append(rate_distribution_err)
+    pickle_object.append(rate_with_multicast)
+    pickle_object.append(rate_without_multicast)
+
+    f_d= outfolder_name+router_id+'.pickle'
+    output_device = open(f_d, 'wb')
+    pickle.dump(pickle_object,output_device)
+    output_device.close()
+
+
 if __name__=='__main__':
     if len(sys.argv) !=6 :
 	print len(sys.argv)
@@ -530,11 +653,11 @@ if __name__=='__main__':
     data_file_header_byte_count=0
     filename_list=[]
     damaged_frames=0
-    unix_time=set()
     #os.system('mkdir -p '+output_folder+router_id )
     print "now processing the files to calculate time "
     #For overall rate distribution of entire home 
     #plot_all_devices_bitrate_distribution(router_id,t1,t2,data_fs)
     #For scatterplot of RSSI vs bitrate and histogram of transmitted to received bitrates    
     #device_rate_vs_rssi_plots(t1,t2,data_fs,router_id)
-    per_station_packet_dumper(t1,t2,data_fs,_folder,router_id)
+    #per_station_packet_dumper(t1,t2,data_fs,_folder,router_id)
+    all_devices_rates_with_multicast_dumper(t1,t2,_folder,data_fs)
